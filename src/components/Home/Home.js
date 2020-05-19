7b6ab75f34d79e56e4a3f250375bb933572021c0
aabubakar <abudawud92@gmail.com>
2020-05-19 06:45:19 +0100

    responsive carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -54,7 +54,7 @@ const Home = () => {
         </div>
         <div class="col-lg-6">          
             <div class="card bradius">
-                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/3762940/pexels-photo-3762940.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/3762940/pexels-photo-3762940.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
                 <div class="card-body pb-5">
                   <p class="card-text">Get answers to your medical questions from the comfort of your home</p>
                 </div>
@@ -63,7 +63,7 @@ const Home = () => {
         </div>
         <div class="col-lg-6">          
             <div class="card bradius">
-                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""  />
                 <div class="card-body pb-5">
                   <p class="card-text">View your health summary <br/> and needed test</p>
                 </div>
@@ -73,7 +73,7 @@ const Home = () => {
         <div class="col-lg-6">
           
             <div class="card bradius">
-                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/162583/work-workplace-office-computer-162583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/162583/work-workplace-office-computer-162583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
                 <div class="card-body pb-5">
                   <p class="card-text">See upcoming appointments</p>
                 </div>
@@ -81,7 +81,7 @@ const Home = () => {
         </div>
         <div class="col-lg-6">          
             <div class="card bradius">
-                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/954583/pexels-photo-954583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                <img class="img-card" style={{height:"200px"}} src="https://images.pexels.com/photos/954583/pexels-photo-954583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
                 <div class="card-body pb-5">
                   <p class="card-text">View lab and imaging test results</p>
                 </div>
@@ -94,91 +94,116 @@ const Home = () => {
 
  
   <section class="call-to-action text-white text-center">    
-    <div class="">
-      <div class="row">
-        <div class="col-md-12">
-          <h2 class="underline">Testimonials</h2>
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">         
-            <div class="carousel-inner">
-              <div class="item carousel-item active">
-                <div class="row">                  
-                  <div class="col-md-5 slant">
-                    <div class="scontent">
-                      <blockquote class="blockquote text-center">
-                        <p class="">See what patients are saying about their experience</p>
-                      
-                        <footer class="blockquote-footer mb-3 text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
-                        <button type="button" class="btn text-light btn-outline-secondary">See more</button>
-                      </blockquote>
-                     
-
+        <div class="">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="underline">Testimonials</h2>
+              <div id="myCarousel" class="carousel slide" data-ride="carousel">         
+                <div class="carousel-inner">
+                  <div class="item carousel-item active">
+                    <div class="row">                  
+                      <div class="col-md-5 slant">
+                        <div class="scontent">
+                          <blockquote class="blockquote text-center">
+                            <p class="">See what patients are saying about their experience</p>
+                          
+                            <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
+                            <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                          </blockquote>
+                         
+    
+                        </div>
+                      </div>
+                      <div class="col-md-7 skewLeft">
+                        <div class="img-box"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Black_woman_with_women%27s_suit_10.jpg/599px-Black_woman_with_women%27s_suit_10.jpg" alt="" /></div>
+                        <div class="overlay-text pl-2 pr-3  pb-2">
+                            <blockquote class="blockquote text-center">
+                              <p class="">See what patients are saying about their experience</p>
+                            
+                              <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the <br/> next best thing </q></footer>
+                              <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                            </blockquote>
+                           
+      
+                          </div>
+                      </div>
                     </div>
                   </div>
-                  <div class="col-md-7 skewLeft">
-                    <div class="img-box"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Black_woman_with_women%27s_suit_10.jpg/599px-Black_woman_with_women%27s_suit_10.jpg" alt="" /></div>
+                  <div class="item carousel-item">
+                    <div class="row">                  
+                      <div class="col-md-5 slant">
+                        <div class="scontent">
+                          <blockquote class="blockquote text-center">
+                            <p class="">See what patients are saying about their experience</p>
+                          
+                            <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
+                            <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                          </blockquote>
+                         
+    
+                        </div>
+                      </div>
+                      <div class="col-md-7 skewLeft">
+                        <div class="img-box"><img src="https://media.glamour.com/photos/5b06fc3c5abbd74fed2e4dfe/16:9/w_2560%2Cc_limit/ervin%2525201.JPG" alt="" /></div>
+                        <div class="overlay-text pl-2 pr-3  pb-2">
+                            <blockquote class="blockquote text-center">
+                              <p class="">See what patients are saying about their experience</p>
+                            
+                              <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the <br/> next best thing </q></footer>
+                              <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                            </blockquote>
+                           
+      
+                          </div>
+                      </div>
+                    </div>
+                
+                    
+                  </div>
+                  <div class="item carousel-item">
+                    <div class="row">                  
+                      <div class="col-md-5 slant">
+                        <div class="scontent">
+                          <blockquote class="blockquote text-center">
+                            <p class="">See what patients are saying about their experience</p>
+                          
+                            <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
+                            <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                          </blockquote>
+                         
+    
+                        </div>
+                      </div>
+                      <div class="col-md-7 skewLeft">
+                        <div class="img-box"><img src="https://images.pexels.com/photos/3768911/pexels-photo-3768911.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" /></div>
+                        <div class="overlay-text pl-2 pr-3  pb-2">
+                            <blockquote class="blockquote text-center">
+                              <p class="">See what patients are saying about their experience</p>
+                            
+                              <footer class="blockquote-footer text-light"> <q class="b"> Nurses may not be angels but they are the <br/> next best thing </q></footer>
+                              <button type="button" class="btn text-light btn-outline-secondary">See more</button>
+                            </blockquote>
+                           
+      
+                          </div>
+                      </div>
+                    </div>
+                    
                     
                   </div>
                 </div>
-              </div>
-              <div class="item carousel-item">
-                <div class="row">                  
-                  <div class="col-md-5 slant">
-                    <div class="scontent">
-                      <blockquote class="blockquote text-center">
-                        <p class="">See what patients are saying about their experience</p>
-                      
-                        <footer class="blockquote-footer mb-3 text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
-                        <button type="button" class="btn text-light btn-outline-secondary">See more</button>
-                      </blockquote>
-                     
-
-                    </div>
-                  </div>
-                  <div class="col-md-7 skewLeft">
-                    <div class="img-box"><img src="https://media.glamour.com/photos/5b06fc3c5abbd74fed2e4dfe/16:9/w_2560%2Cc_limit/ervin%2525201.JPG" alt="" /></div>
-                    
-                  </div>
-                </div>
-            
-                
-              </div>
-              <div class="item carousel-item">
-                <div class="row">                  
-                  <div class="col-md-5 slant">
-                    <div class="scontent">
-                      <blockquote class="blockquote text-center">
-                        <p class="">See what patients are saying about their experience</p>
-                      
-                        <footer class="blockquote-footer mb-3 text-light"> <q class="b"> Nurses may not be angels but they are the next best thing </q></footer>
-                        <button type="button" class="btn text-light btn-outline-secondary">See more</button>
-                      </blockquote>
-                     
-
-                    </div>
-                  </div>
-                  <div class="col-md-7 skewLeft">
-                    <div class="img-box"><img src="https://images.pexels.com/photos/3768911/pexels-photo-3768911.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" /></div>
-                    
-                  </div>
-                </div>
-                
-                
+               
+                <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+                </a>
               </div>
             </div>
-           
-            <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
-              <i class="fa fa-angle-left"></i>
-            </a>
-            <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
-              <i class="fa fa-angle-right"></i>
-            </a>
           </div>
         </div>
-      </div>
-    </div>
-  </section>
-
- 
+      </section>
   <footer class="footer">
     <div class="container">
       <div class="row">
