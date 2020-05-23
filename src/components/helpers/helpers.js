4732d274f5f70aba0f728ae43660d206d4c4e609
nwachukwu <nugwuanyi@bluetag-it.com>
2020-05-23 02:06:23 +0100

    updated time configuration for the request date-time

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -11,10 +11,10 @@ export const User = () =>{
     return JSON.parse(localStorage.getItem("user"))
 }
 export const getTime = (date) =>{
-    let time =  new Date(date).toLocaleTimeString().split(":")    
-    if(parseInt(time[0]) <= 12){
-        return time[0] + ":" + time[1] + ' am'
-    }else{
-        return  parseInt(time[0]) - 12 + ":" + time[1] + ' pm'
-    }
+    let dt =  new Date(date);   
+
+    var h =  dt.getHours(), m = dt.getMinutes();
+    var _time = (h > 12) ? (h-12 + ':' + m +' pm') : (h + ':' + m +' am');
+
+    return _time;
 } 
